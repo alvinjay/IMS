29bab8a2a74b9472e952b71cdcbc3d9abe926ed6
Alvin Jay Cosare <ajunecosare15@gmail.com>
2015-02-14 13:53:51 +0800

    Initial dist

---
+++ server/config/environment/development.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    //uri: 'mongodb://localhost/imscosare-dev'
+    uri: 'mongodb://alvinjay:walakokabalo@ds045001.mongolab.com:45001/mongo-sp'
+  },
+
+  seedDB: true
+};
